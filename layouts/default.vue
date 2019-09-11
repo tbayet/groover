1486fe2623a80b3d8ae97644e060cac550372881
tbayet <tbayet@vente-privee.com>
2019-09-11 14:58:46 +0200

    meta/boilerplate
    init project with Nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn>
+        Become an Influencer
+      </v-btn>
+      <v-btn>
+        Log In
+      </v-btn>
+      <v-btn>
+        Sign up
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      title: 'Groover'
+    }
+  }
+}
+</script>
